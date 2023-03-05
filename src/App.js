c8a4f2a6e1568a1b02b46a68207d16cb2e2f791a
Andrey Dmitriev <admit8490@gmail.com>
2023-03-05 22:43:23 +0200

    initial dice state, reroll button static

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+import { useState } from "react";
+
 import Die from "./components/Die";
 
 function App() {
+
+  // helper functions
+
+  const getRandomNumber = (range) => {
+    return Math.ceil(Math.random() * range);
+  }
+
+  const getDicevalues = (diceNumber, range) => {
+    return Array(diceNumber)
+      .fill(null)
+      .map(item => getRandomNumber(range));
+  }
+
+  // state
+
+  const [dice, setDice] = useState(() => getDicevalues(10, 6))
+
+  // rendering
+  
+  let diceElements = dice.map((value, index) => <Die value={value} key={index} />);
+  
   return (
    <main>
     <div className="die-container">
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
-      <Die value="1"/>
+      {diceElements}
     </div>
+    <button className="reroll">Roll</button>
    </main>
   );
 }
